6732334bc8b6e9aa3a84b3470df192e886cb0574
22130107 <22130107@st.hcmuaf.edu.vn>
2026-07-27 21:25:28 +0700

    Upgrade Next.js 16 fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack(config) {
-    const fileLoaderRules = config.module.rules
-      .flatMap((rule: any) => (rule.oneOf ? rule.oneOf : rule))
-      .filter((rule: any) => rule?.test instanceof RegExp && rule.test.test(".png"));
-
-    for (const rule of fileLoaderRules) {
-      if (rule.type === "asset") {
-        rule.type = "asset/resource";
-      }
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
